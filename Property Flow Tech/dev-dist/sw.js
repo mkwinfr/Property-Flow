77b7aab903b017b68e3485baa188b3ecb965c1c5
mkwinfr <mkwinfr@gmail.com>
2025-12-09 23:15:25 -0600

    working 1.234

--- Property Flow Tech/dev-dist/sw.js
+++ Property Flow Tech/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9c42aiao8o8"
+    "revision": "0.7nsn8q3ndsg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
