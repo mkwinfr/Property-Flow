e0ed18ae95d8fdb51b2464601d37c18f805a8da2
mkwinfr <mkwinfr@gmail.com>
2025-12-12 07:38:24 -0600

    1

--- Property Flow Backend/src/index.ts
+++ Property Flow Backend/src/index.ts
@@ -46,7 +46,7 @@ app.get("/", (req, res) => {
   res.send("Property Flow Backend Running");
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = Number(process.env.PORT) || 4000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
