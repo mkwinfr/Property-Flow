081c629178984bc3b5e88e0b7952cc82653ffdf6
mkwinfr <mkwinfr@gmail.com>
2025-12-19 23:07:24 -0600

    1

--- Launcher/Source/obj/Debug/net8.0-windows/PropertyFlow.Launcher.AssemblyInfo.cs
+++ Launcher/Source/obj/Debug/net8.0-windows/PropertyFlow.Launcher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PS Launcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3775182fca5ed1f0cd99b8740e63d797f2a3f69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eb343b4e02cb36d62bd3a5257a60eef1c447fb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PS Launcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PS Launcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
